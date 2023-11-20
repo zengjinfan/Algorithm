d8b5a7cdb034f0e09529ef42947f907e70e1734b
zengjinfan <zengjinfan@black-unique.com>
2023-11-20 10:27:36 +0800

    修正程序

--- src/main/java/leetcode/QskMpc202311获取最多的公仔玩具/QskMpc202311.java
+++ src/main/java/leetcode/QskMpc202311获取最多的公仔玩具/QskMpc202311.java
@@ -29,7 +29,7 @@ public class QskMpc202311 {
         for (int i = searchStart; i < candidateList.size(); i++) {
             Integer candidate = candidateList.get(i);
             if(i > searchStart && candidate.equals(candidateList.get(i - 1))){
-                return;
+                continue;
             }
             currentList.add(candidate);
             dfs(currentList, candidateList, i + 1, target - candidate);
@@ -38,41 +38,41 @@ public class QskMpc202311 {
     }
 
     public static void main(String[] args) {
-        Integer[] dollArr = {4, 3, 7};
+        Integer[] dollArr = {3, 9, 15, 7, 14, 3, 9, 14, 7}; //3,3,7,7,9,9,14,14,15
         List<Integer> dollList = Arrays.asList(dollArr);
-        int luckyNum = 7;
+        int luckyNum = 48;
         int res = new QskMpc202311().maxDollCount(dollList, luckyNum);
         System.out.println(res);
 
-        Integer[] dollArr2 = {1, 1, 2, 2, 3, 3};
-        dollList = Arrays.asList(dollArr2);
-        luckyNum = 6;
-        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
-        System.out.println(res);
+//        Integer[] dollArr2 = {1, 1, 2, 2, 3, 3};
+//        dollList = Arrays.asList(dollArr2);
+//        luckyNum = 6;
+//        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
+//        System.out.println(res);
+//
+//        Integer[] dollArr3 = {2, 2, 3, 3};
+//        dollList = Arrays.asList(dollArr3);
+//        luckyNum = 1;
+//        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
+//        System.out.println(res);
 
-        Integer[] dollArr3 = {2, 2, 3, 3};
-        dollList = Arrays.asList(dollArr3);
-        luckyNum = 1;
-        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
-        System.out.println(res);
-
-        Integer[] dollArr4 = {6,5,4,3,2,1};
-        dollList = Arrays.asList(dollArr4);
-        luckyNum = 9;
-        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
-        System.out.println("检查1：" + res);
-
-        Integer[] dollArr5 = {2,2,3,3,4,4,5,5,6,8,9,10,12,13,15,17,16,22,15,13,16};
-        dollList = Arrays.asList(dollArr5);
-        luckyNum = 55;
-        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
-        System.out.println("检查2：" + res);
-
-        Integer[] dollArr6 = {7,4,7,2,9,2,9,7,1,5,8,5,8,5,3,6,6,7,8,1,1,6,2,1,6,4,9,8,7,5,6,2,8,8,1,8,2,8,1,4,7,5,3,8,7,7,6,2,1,4,4,1,8,3,9,3,9,1,9,8,2,5,1,4,3,4,6,6,8,8,4,1,9,5,2,5,5,2,5,2,6,2,6,9,8,6,6,9,2,9,6,1,5,1,8,1,1,8,1,9,2,8,8,1,1,7,7,4,9};
-        dollList = Arrays.asList(dollArr6);
-        luckyNum = 66;
-        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
-        System.out.println("检查3：" + res);
+//        Integer[] dollArr4 = {6,5,4,3,2,1};
+//        dollList = Arrays.asList(dollArr4);
+//        luckyNum = 9;
+//        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
+//        System.out.println("检查1：" + res);
+//
+//        Integer[] dollArr5 = {2,2,3,3,4,4,5,5,6,8,9,10,12,13,15,17,16,22,15,13,16};
+//        dollList = Arrays.asList(dollArr5);
+//        luckyNum = 55;
+//        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
+//        System.out.println("检查2：" + res);
+//
+//        Integer[] dollArr6 = {7,4,7,2,9,2,9,7,1,5,8,5,8,5,3,6,6,7,8,1,1,6,2,1,6,4,9,8,7,5,6,2,8,8,1,8,2,8,1,4,7,5,3,8,7,7,6,2,1,4,4,1,8,3,9,3,9,1,9,8,2,5,1,4,3,4,6,6,8,8,4,1,9,5,2,5,5,2,5,2,6,2,6,9,8,6,6,9,2,9,6,1,5,1,8,1,1,8,1,9,2,8,8,1,1,7,7,4,9};
+//        dollList = Arrays.asList(dollArr6);
+//        luckyNum = 66;
+//        res = new QskMpc202311().maxDollCount(dollList, luckyNum);
+//        System.out.println("检查3：" + res);
     }
 
     /*
